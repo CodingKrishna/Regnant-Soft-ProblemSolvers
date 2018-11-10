135a8cb7d8b2d8de492aef7f77086caf6a4aa3e7
vikram133 <vikram4adapa@gmail.com>
2018-11-10 18:18:14 +0530

    my maths programs

---
+++ maths_lab/exponential.js
@@ -0,0 +1,13 @@
+/**
+     * exponential
+     */
+    
+    console.log(k);
+    var e=2.17;
+    var n=2;
+    var k=1;
+    while(n>0){
+        k=k*e;
+        n--;
+    }
+    console.log("exponentialll"+k)
